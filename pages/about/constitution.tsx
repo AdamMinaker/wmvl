72ff32533b4dc6e73f405034d0c509b7fe9b0532
Adam Minaker <adamminaker@outlook.com>
2023-01-29 14:16:09 -0600

    Update constitution

--- pages/about/constitution.tsx
+++ pages/about/constitution.tsx
@@ -21,12 +21,15 @@ export default function Home() {
       <main className='flex-1'>
         <div className='py-6'>
           <div className='mx-auto max-w-7xl px-4 sm:px-6 md:px-8'>
-            <h1 className='text-2xl font-semibold dark:text-[#d8d4cf] text-gray-900'>Home</h1>
+            <h1 className='text-2xl font-semibold dark:text-[#d8d4cf] text-gray-900'>Constitution</h1>
           </div>
           <div className='mx-auto max-w-7xl px-4 sm:px-6 md:px-8'>
             <div className='py-4'>
               <p>
-               Consitiution goes here.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.
               </p>
             </div>
           </div>
